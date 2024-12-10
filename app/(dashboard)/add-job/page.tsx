192b0bf7ff0b4bedef5e6f66ed986116957e2617
zreonx <zrodayrit@gmail.com>
2024-12-10 11:24:12 +0800

    added shadcn form setups and types setup with zod

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,5 +1,11 @@
+import CreateJobForm from '@/components/CreateJobForm';
+
 function AddJobPage() {
-  return <h1 className="text-4xl">AddJobPage</h1>;
+  return (
+    <>
+      <CreateJobForm />
+    </>
+  );
 }
 
 export default AddJobPage;
